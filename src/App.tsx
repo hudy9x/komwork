cb4818794286ac2052def6c4d4254364651fb5f9
hudy9x <hudy9x@gmail.com>
2022-11-16 01:23:06 +0700

    style: add detail ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import ProjectArea from "./containers/ProjectArea";
 import Sidebar from "./containers/Sidebar";
+import TaskDetail from "./containers/TaskDetail";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
       <Sidebar />
       <main id="main-content" className="w-screen">
         <ProjectArea/>
+        <TaskDetail />
       </main>
     </div>
   );
---
+++ src/containers/TaskDetail.tsx
@@ -0,0 +1,5 @@
+export default function TaskDetail() {
+  return <div className="task-detail">
+
+  </div>
+}
